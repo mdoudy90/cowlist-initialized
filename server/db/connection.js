b8257dfda72fe85ecd521bc83747eee6cc057f42
Michael Doudy <michael.doudy@gmail.com>
2020-06-07 12:38:20 -0400

    Set up db schema

--- server/db/connection.js
+++ server/db/connection.js
@@ -4,9 +4,26 @@ const connection = mysql.createConnection({
   host : 'localhost',
   user: 'root',
   password: '',
+  multipleStatements: true,
 })
 
 connection.connect();
+
+// SCHEMA SETUP
+connection.query(`
+  CREATE DATABASE IF NOT EXISTS cow_list;
+
+  USE cow_list;
+
+  CREATE TABLE IF NOT EXISTS
+  cows (id INT(11) AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(30),
+        description VARCHAR(255));
+`, (error, data) => {
+  if (error) {
+    throw error;
+  }
+});
 
 connection.end();
 
