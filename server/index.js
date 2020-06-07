9d7703b128e190e2cf2e968436b1809824861b3f
Michael Doudy <michael.doudy@gmail.com>
2020-06-07 11:21:41 -0400

    Installed body-parser and set up middleware for json and urlencoded body parsing

--- server/index.js
+++ server/index.js
@@ -1,9 +1,32 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+
 const app = express()
 const port = 3000
 
 app.use(express.static('./client/dist'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+
+
+/*
+
+--- Backend Layout ---
+Routes
+- Request types
+Models
+- Handling
+Controllers
+- Handlers (SQL)
+DB
+- mysql connection
+
+--- Frontend Layout ---
+
+*/
